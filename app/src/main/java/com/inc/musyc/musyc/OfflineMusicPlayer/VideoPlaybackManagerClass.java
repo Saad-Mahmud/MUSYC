39d57170169ac5ff4a388e71826e20b2c2c465e7
saad <saadmahmud14@gmail.com>
2017-11-07 19:08:57 +0600

    Initial commit

---
+++ app/src/main/java/com/inc/musyc/musyc/OfflineMusicPlayer/VideoPlaybackManagerClass.java
@@ -0,0 +1,16 @@
+package com.inc.musyc.musyc.OfflineMusicPlayer;
+
+//Controls video playback
+
+public class VideoPlaybackManagerClass
+{
+    static void playVideo(String videoPath)
+    {
+        KaraokeHubActivity.karaokeVideoView.setVideoPath(videoPath);      //Starts playing video using video path sent as parameter
+        KaraokeHubActivity.karaokeVideoView.start();
+    }
+    static void pauseVideo()
+    {
+        KaraokeHubActivity.karaokeVideoView.pause();
+    }   //Pauses video
+}
